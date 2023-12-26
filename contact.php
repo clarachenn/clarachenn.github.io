d29403c4bfb65a6a5800e87b87a9db4116e7024d
Clara Chen <59670258+clarachenn@users.noreply.github.com>
2023-12-26 15:48:29 -0800

    Edited contact

--- contact.php
+++ contact.php
@@ -1,24 +1,11 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-        $to = "claracjx@gmail.com";
-        $subject = "New Contact Form Submission";
-
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n";
-        $email_content .= "Subject: $subject\n\n";
-        $email_content .= "Message:\n$message";
-
-        $headers = "From: $email";
-
-        if (mail($to, $subject, $email_content, $headers)) {
-            echo "Success: Email sent successfully";
-            exit;
-        } else {
-            echo "Error: Email could not be sent";
-        }
-    }
+    // Log the form data to a file for testing purposes
+    $logFile = fopen("form_data_log.txt", "a");
+    fwrite($logFile, "Name: $name\nEmail: $email\nMessage: $message\n\n");
+ }
 ?>
